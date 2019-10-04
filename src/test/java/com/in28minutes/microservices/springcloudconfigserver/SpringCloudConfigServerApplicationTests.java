16b95b45819cd4acb820f3e6f80799e4d8c5f9ab
ardi priasa <ardi.priasa@gmail.com>
2019-10-04 15:23:46 +0700

    import source code

---
+++ src/test/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.in28minutes.microservices.springcloudconfigserver;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Ignore
+public class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
